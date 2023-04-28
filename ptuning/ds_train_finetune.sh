94432b8bd550e0475baa7f527a43b2a78a72c6f5
jamsluo <jamsluo@tencent.com>
2023-04-28 18:52:53 +0800

    init chatglm

--- ptuning/ds_train_finetune.sh
+++ ptuning/ds_train_finetune.sh
@@ -1,5 +1,5 @@
 
-LR=1e-4
+LR=1e-6
 DATESTR=$(date +"%m-%d-%H-%M")
 MASTER_PORT=$(shuf -n 1 -i 10000-65535)
 
@@ -10,18 +10,18 @@ nohup deepspeed --num_gpus=8 --master_port $MASTER_PORT main.py \
     --test_file dataset/rc_0422_new/dev.json \
     --prompt_column content \
     --response_column summary \
-    --model_name_or_path /search/ai/pretrain_models/chatglm-6b-dropout/ \
-    --output_dir ./output/rc-1536-chatglm-6b-dropout-$DATESTR \
+    --model_name_or_path /search/ai/pretrain_models/chatglm-6b/ \
+    --output_dir ./output/rc-1536-chatglm-6b-lowlr-$DATESTR \
     --overwrite_output_dir \
     --max_source_length 1536 \
     --max_target_length 512 \
     --per_device_train_batch_size 10 \
     --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 4 \
     --predict_with_generate \
-    --max_steps 6000 \
+    --max_steps 2000 \
     --logging_steps 10 \
-    --save_steps 500 \
+    --save_steps 200 \
     --learning_rate $LR \
     --lr_scheduler_type "cosine" \
     --fp16 > logs/log.log-$DATESTR &
--- ptuning/train_pturining.sh
+++ ptuning/train_pturining.sh
@@ -1,4 +1,4 @@
-LR=1e-3
+LR=1e-5
 PRE_SEQ_LEN=128
 DATESTR=$(date +"%m-%d-%H-%M")
 MASTER_PORT=$(shuf -n 1 -i 10000-65535)
@@ -11,15 +11,15 @@ nohup deepspeed --num_gpus=8 --master_port $MASTER_PORT main.py \
     --prompt_column content \
     --response_column summary \
     --model_name_or_path /search/ai/pretrain_models/chatglm-6b/ \
-    --output_dir ./output/rc-1536-chatglm-6b-$PRE_SEQ_LEN-$LR-$DATESTR \
+    --output_dir ./output/rc-1536-chatglm-lowlr-ptuning-$PRE_SEQ_LEN-$LR-$DATESTR \
     --overwrite_output_dir \
     --max_source_length 1536 \
     --max_target_length 512 \
     --per_device_train_batch_size 12 \
     --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 2 \
     --predict_with_generate \
-    --max_steps 6000 \
+    --max_steps 3000 \
     --logging_steps 10 \
     --save_steps 500 \
     --learning_rate $LR \
